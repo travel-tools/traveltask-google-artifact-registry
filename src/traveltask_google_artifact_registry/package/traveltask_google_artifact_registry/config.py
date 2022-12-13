dc4afc385ddf04c60ecabd515170726f0eca4e97
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2022-12-13 17:17:56 +0100

    Update config.py

--- src/traveltask_google_artifact_registry/package/traveltask_google_artifact_registry/config.py
+++ src/traveltask_google_artifact_registry/package/traveltask_google_artifact_registry/config.py
@@ -12,5 +12,5 @@ class TaskConfig:
     project: str
     region: str
     repository: str
-    action: str  # Can be upload or install
+    action: str  # Can be upload or install or fix_requirements
     packages: str = None
