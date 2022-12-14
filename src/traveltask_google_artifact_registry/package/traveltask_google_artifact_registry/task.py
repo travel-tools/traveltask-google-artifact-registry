50a0c78cf9e6842a0df1b7bd5794a093762e33f5
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2022-12-14 19:25:36 +0100

    Add `fix_requirements` to scoped venvs as well

--- src/traveltask_google_artifact_registry/package/traveltask_google_artifact_registry/task.py
+++ src/traveltask_google_artifact_registry/package/traveltask_google_artifact_registry/task.py
@@ -3,6 +3,7 @@ import sys
 
 from travel.config.bag import Bag
 from travel.config.reader import parse_bags
+from travel.tools.base_venv import BaseVirtualenv
 from travel.tools.pip import Pip
 from travel.tools.python import Python
 from travel.tools.venv import Virtualenv
@@ -17,12 +18,10 @@ def _get_index_url(config: TaskConfig):
     return f"https://{config.region}-python.pkg.dev/{config.project}/{config.repository}/"
 
 
-def _fix_requirements(bag: Bag, python: Python, config: TaskConfig) -> None:
-    # Get the right venv
-    env = Virtualenv(bag)
-    
+def _fix_venv_requirements(env: BaseVirtualenv, index_url: str) -> None:
     if not os.path.exists(env.path):
         raise ValueError(
+            f"Non-existent venv: {env._name}. "
             "The venv must be created before running the 'fix_requirements' action. "
             "Did you forget to run 'travel setup'?"
         )
@@ -30,16 +29,37 @@ def _fix_requirements(bag: Bag, python: Python, config: TaskConfig) -> None:
     with open(env.requirements_file, "rt") as f:
         requirements = f.read().splitlines()
         
-    if PyPI_keyrings_google_artifact_registry_auth not in requirements:
+    has_keyrings = False
+    
+    for requirement in requirements:
+        if PyPI_keyrings_google_artifact_registry_auth in requirement:
+            has_keyrings = True
+            break
+
+    if not has_keyrings:
         requirements.append(PyPI_keyrings_google_artifact_registry_auth)
         
-    index_url = _get_index_url(config) + "simple"
     requirements.insert(0, f"--extra-index-url {index_url}")
     
     with open(env.requirements_file, "wt") as f:
         for requirement in requirements:
             f.write(f"{requirement}\n")
 
+
+def _fix_requirements(bag: Bag, python: Python, config: TaskConfig) -> None:
+    index_url = _get_index_url(config) + "simple"
+    
+    # It fixes the main venv
+    env = Virtualenv(bag)
+    _fix_venv_requirements(env, index_url)
+    
+    if bag.scopes:
+        scopes = ScopedVirtualenvs(bag)
+        
+        # It fixes scoped venvs
+        for scoped_env in scopes.envs.values():
+            _fix_venv_requirements(scoped_env, index_url)
+ 
 
 def _install(bag: Bag, python: Python, config: TaskConfig):
 
